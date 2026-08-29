cf921d30bcf0a2c68cf58a08e0f6b614a3891155
Mathias Stearn <redbeard0531@gmail.com>
2026-08-29 15:13:25 +0000

    tools: add 'All off' button next to Reset to defaults in the tool popover

--- ui/src/vue/components/ChatInterface.vue
+++ ui/src/vue/components/ChatInterface.vue
@@ -1452,6 +1452,16 @@ function resetToolOverrides() {
     /* ignore */
   }
 }
+function allToolsOff() {
+  const next: Record<string, "off"> = {};
+  for (const tool of availableTools.value) next[tool.name] = "off";
+  toolOverrides.value = next;
+  try {
+    localStorage.setItem(TOOL_OVERRIDES_KEY, JSON.stringify(next));
+  } catch {
+    /* ignore */
+  }
+}
 const toolOverrideCount = computed(() => Object.keys(toolOverrides.value).length);
 
 const toolOverrideList = computed(() => availableTools.value);
@@ -3734,6 +3744,7 @@ const statusContentProps = computed(() => {
     onThinkingChange: setThinkingLevel,
     onSetToolOverride: setToolOverride,
     onResetToolOverrides: resetToolOverrides,
+    onAllToolsOff: allToolsOff,
     onOpenDirectoryPicker: () => (showDirectoryPicker.value = true),
     onUsageNeeded: () => (usageWanted.value = true),
   };
